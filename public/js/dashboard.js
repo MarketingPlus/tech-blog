3608aa4e02e7f6f86388c9594a2cf6463fcf66b3
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 13:16:50 +1000

    hide and show the create blog form

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,3 +6,16 @@ if (!blogContainer) {
         .querySelector('#no-blogs-message')
         .classList.remove('hidden')
 }
+
+// show & hide form to create a blog 
+function openForm() {
+    document
+        .querySelector('#form-container')
+        .classList.remove('hidden');
+};
+
+function closeForm() {
+    document
+        .querySelector('#form-container')
+        .classList.add('hidden');
+};
