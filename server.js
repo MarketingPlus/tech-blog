229e2ac4ce2bedb3c6422fb07899cd7f0c876264
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 12:42:46 +1000

    added my session information to my server.js file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
+const express = require('express'); 
 const routes = require('./controllers');
+const sequelize = require('./config/connection'); 
+const path = require('path'); 
+const helpers = require('./utils/helpers'); 
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// handlebars
+const exphbs = require('express-handlebars'); 
+const hbs = exphbs.create( { helpers }); 
+
+// session
+const session = require('express-session'); 
+const SequelizeStore = require('connect-session-sequelize')(session.Store); 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; 
