bd81a81855408b0c6df425e36b481e9462ab193e
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 13:34:23 +1000

    added the delete request with auth

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -13,3 +13,23 @@ router.post('/', withAuth, async (req, res) => {
         res.status(400).json(err); 
     }
 }); 
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id, 
+            }
+        }); 
+
+        if (!commentData) {
+            res.status(404).json({ message: 'no comment found with that id' });
+            return; 
+        }; 
+
+        res.status(200).json(commentData); 
+    } catch (err) {
+        res.status(500).json(err); 
+    }
+});
