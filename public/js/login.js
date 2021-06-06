14a0597025e41aad1539c03b53e25179a8a93ad4
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 13:13:21 +1000

    added a function for signing up

--- public/js/login.js
+++ public/js/login.js
@@ -22,6 +22,30 @@ const loginFormHandler = async (event) => {
         alert(error.message)
       }
     } else {
-      alert('please enter a username & password')
+      alert('Please enter a username & password')
     }
   };
+
+  //function for signing up
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to sign up.');
+      }
+    } else {
+      alert('please enter a username & password')
+    };
+  };
+  
