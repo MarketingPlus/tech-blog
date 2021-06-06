3452fe41482a643b9ae49f3daba2d1b28e6e4d75
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 13:22:40 +1000

    check the password for the user

--- models/User.js
+++ models/User.js
@@ -1,3 +1,10 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcryptjs');
+
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password); 
+    }
+  }
+  
