9700d02495d95be3a2683143e005db66bdfb7331
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 13:15:14 +1000

    added the function so you can create a new blog post

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -10,3 +10,28 @@ function closeForm() {
         .querySelector('#form-container')
         .classList.add('hidden');
 };
+
+// create a new blog 
+const createNewBlog = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-input-home').value.trim(); 
+    const text = document.querySelector('#text-input-home').value.trim();
+
+    
+    if (title && text) {
+        const response = await fetch('/api/blog', {
+          method: 'POST',
+          body: JSON.stringify({ title, text }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+    
+        if (response.ok) {
+          document.location.reload(); 
+        } else {
+          alert('Failed to create blog post');
+        }
+    }
+};
