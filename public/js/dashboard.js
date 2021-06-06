d8f86739ad3e83e0a8eecee38655fff30eeb1208
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 13:17:16 +1000

    added the create new blog post to the dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -19,3 +19,28 @@ function closeForm() {
         .querySelector('#form-container')
         .classList.add('hidden');
 };
+
+// create a new blog post
+const createNewBlog = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#title-input').value.trim(); 
+    const text = document.querySelector('#text-input').value.trim();
+
+    
+    if (title && text) {
+        const response = await fetch('/api/blog', {
+          method: 'POST',
+          body: JSON.stringify({ title, text }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+    
+        if (response.ok) {
+          document.location.reload(); 
+        } else {
+          alert('Failed to create blog post');
+        }
+    }
+};
