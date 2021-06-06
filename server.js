89ccb0705e7653c36804b468fae30bd5a802a586
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 12:44:24 +1000

    sets Handlebars as the default template engine

--- server.js
+++ server.js
@@ -26,3 +26,11 @@ const sess = {
 }; 
 
 app.use(session(sess));
+
+//set Handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars'); 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.static(path.join(__dirname, 'public'))); 
