a39c25f7e12ef52e06521d018863db1ec8f15b6f
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 13:28:23 +1000

    added the schema sql file to create the database

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+-- jawsdb schema for heroku deployment
+DROP DATABASE IF EXISTS techblog_db;
+CREATE DATABASE techblog_db;
+
+-- use this schema for local host
+DROP DATABASE IF EXISTS techblog_dblocal;
+CREATE DATABASE techblog_dblocal;
