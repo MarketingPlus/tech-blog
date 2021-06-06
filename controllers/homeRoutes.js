355ff570e52b82641d934411d16886bd57b6629e
MarketingPlus <benmarketingplus@gmail.com>
2021-06-06 13:29:54 +1000

    added the homeage controller which renders the homepage with blog posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
 const { Blog, Comment, User } = require('../models');
 const withAuth = require('../utils/auth'); 
+
+// render homepage with blog posts 
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            include: [
+                {
+                    model: User, 
+                },
+            ], 
+        }); 
+        const blogs = blogData.map((blog) => blog.get({ plain: true })); 
+
+        res.render('homepage', {
+            layout: 'main',
+            logged_in: req.session.logged_in, 
+            blogs, 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}); 
